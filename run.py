0c44109313aa4f7ee589706968f8f12cdda257bf
Kim Bahir Andersen <kim@bahir.dk>
2021-07-02 16:46:17 +0200

    Commit before deletion

--- run.py
+++ run.py
@@ -9,11 +9,11 @@ $env:TADO_EMAIL = "<email>"
 Used for quick testing of the class
 """
 
-# tado_pass = os.environ['TADO_PASS']
-# tado_email = os.environ['TADO_EMAIL']
+tado_pass = os.environ['TADO_PASS']
+tado_email = os.environ['TADO_EMAIL']
 
-tado_email = "blah@blah.com"
-tado_pass = "blahblah"
+# tado_email = "blah@blah.com"
+# tado_pass = "blahblah"
 
 t = Tado(tado_email, tado_pass)
 
